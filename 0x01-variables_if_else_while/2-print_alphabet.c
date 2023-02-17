4111c8bd69b70e0ebad9cb2508a3216bc075e81e
Christopher Sammuel <christophersamim@gmail.com>
2023-02-17 01:37:18 -0800

    Print out alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,39 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <time.h>
-
-
-
 /**
+ * main - Prints the alphabet.
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-		char low;
-
-
-
-			for (low = 'a'; low <= 'z'; low++)
-
-						putchar(low);
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
+
